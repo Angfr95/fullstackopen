a2d4da732f021fcf3c5eeb9ed5196cd02ef13f14
Angfr95 <cartebleue34@gmail.com>
2026-03-22 03:49:01 +0100

    phonebook manque réactu

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,121 +1,124 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState, useEffect } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import PersonsList from "./components/PersonsList";
+import {
+  getPersons,
+  addPersons,
+  deletePerson,
+  updatePerson,
+} from "./services/persons";
+import Notification from "./components/Notification";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [personsFilter, setPersonsFilter] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [message, setMessage] = useState(null);
+  const [notificationStyle, setNotificationStyle] = useState({});
+
+  useEffect(() => {
+    getPersons().then((response) => {
+      setPersons(response);
+      setPersonsFilter(response);
+    });
+  }, []);
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const elementFound = persons.find((element) => element.name == newName);
+    if (elementFound !== undefined) {
+      const confirmUpdate = window.confirm(
+        `${newName} is already added to phonebook, Do you want update information?`,
+      );
+      if (confirmUpdate) {
+        const personModify = { ...elementFound, number: newNumber };
+        updatePerson(elementFound.id, personModify);
+        // Notification message
+        setMessage(`Changed number of ${elementFound.name}`);
+        setNotificationStyle({ color: "green" });
+        setTimeout(() => {
+          setMessage(null);
+        }, 5000);
+      }
+    } else {
+      const newId = persons.length + 1;
+      const personToAdd = {
+        name: newName,
+        number: newNumber,
+        id: String(newId),
+      };
+      addPersons(personToAdd).then(() => {
+        setNewName("");
+        setNewNumber("");
+        // Notification message
+        setMessage(`Added ${personToAdd.name}`);
+        setNotificationStyle({ color: "green" });
+        setTimeout(() => {
+          setMessage(null);
+        }, 5000);
+      });
+    }
+  };
+
+  const inputName = (event) => {
+    event.preventDefault();
+    setNewName(event.target.value);
+  };
+  const inputNumber = (event) => {
+    event.preventDefault();
+    setNewNumber(event.target.value);
+  };
+  const inputFilter = (event) => {
+    event.preventDefault();
+    setPersonsFilter(
+      persons.filter((el) =>
+        el.name.toLowerCase().includes(event.target.value.toLowerCase()),
+      ),
+    );
+  };
+
+  const clickDelete = (person) => {
+    if (window.confirm(`Do you really want to delete ${person.name}?`)) {
+      deletePerson(person.id)
+        .then(() => {
+          // Notification message
+          setMessage(`Deleted ${person.name}`);
+          setNotificationStyle({ color: "red" });
+          setTimeout(() => {
+            setMessage(null);
+          }, 5000);
+        })
+        .catch(() => {
+          setMessage(
+            `Information of '${person.name}' was already removed from server`,
+          );
+          setNotificationStyle({ color: "red" });
+          setTimeout(() => {
+            setMessage(null);
+          }, 5000);
+        });
+    }
+  };
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <h2>Phonebook</h2>
+      <Notification styles={notificationStyle} message={message} />
+      <Filter onChangeHandle={inputFilter} />
+      <h3>add a new</h3>
+      <PersonForm
+        submitForm={addPerson}
+        nameValue={newName}
+        nameHandle={inputName}
+        numberValue={newNumber}
+        numberHandle={inputNumber}
+      />
+      <h2>Numbers</h2>
+      <PersonsList personArray={personsFilter} deleteHandle={clickDelete} />
+    </div>
+  );
+};
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
-
-export default App
+export default App;
